02c498ea1a9756bd2f0d4119b911de20dbb9485c
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-12-01 12:36:22 +0100

    amelioration du la calculatrice

--- components/calculator.jsx
+++ components/calculator.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import styles from "@/app/page.module.css";
+import { evaluate } from "mathjs";
 
 export default function Calculator() {
 	const [calculation, setCalculation] = useState("");
@@ -28,26 +29,36 @@ export default function Calculator() {
 	};
 
 	const handleSquareRoot = () => {
-		setCalculation((prevCalculation) => {
-			try {
-				const result = Math.sqrt(eval(prevCalculation)).toString();
-				return result;
-			} catch (error) {
-				return "Error";
-			}
-		});
-	};
+        setCalculation((prevCalculation) => {
+          try {
+            const result = Math.sqrt(evaluate(prevCalculation)).toString(); //const result = Math.sqrt(eval(prevCalculation)).toString();
+            return result;
+          } catch (error) {
+            return "Error";
+          }
+        });
+      };
 
-	const handleSquare = () => {
-		setCalculation((prevCalculation) => {
-			try {
-				const result = (eval(prevCalculation) ** 2).toString();
-				return result;
-			} catch (error) {
-				return "Error";
-			}
-		});
-	};
+      const isNegative = () => {
+        try {
+          // Utiliser math.js pour évaluer l'expression actuelle
+          const value = evaluate(calculation);
+          return value < 0;
+        } catch (error) {
+          return false;
+        }
+      };
+
+      const handleSquare = () => {
+        setCalculation((prevCalculation) => {
+          try {
+            const result = (evaluate(prevCalculation) ** 2).toString();
+            return result;
+          } catch (error) {
+            return "Error";
+          }
+        });
+      };
 
 	const handleOperator = (operator) => {
 		setCalculation((prevCalculation) => {
@@ -73,16 +84,16 @@ export default function Calculator() {
 		});
 	};
 
-	const calculateResult = () => {
-		try {
-			const calculatedResult = eval(calculation).toString();
-			setResult(calculatedResult);
-			setCalculation(calculatedResult);
-			setDisplayedResults(true);
-		} catch (error) {
-			setResult("Error");
-		}
-	};
+    const calculateResult = () => {
+        try {
+          const calculatedResult = evaluate(calculation).toString(); 
+          setResult(calculatedResult);
+          setCalculation(calculatedResult);
+          setDisplayedResults(true);
+        } catch (error) {
+          setResult("Error");
+        }
+      };
 
 	const clearAll = () => {
 		setCalculation("");
@@ -108,34 +119,36 @@ export default function Calculator() {
 
 	return (
 		<div className={styles.calculator}>
-			<div className={styles.display}>
-				<p className={styles.calculation}>{calculation}</p>
-				<p className={styles.result}>{result || 0}</p>
-			</div>
+			<div className={`${styles.display} ${result === "Error" ? styles.error : ""}`}>
+  <p className={styles.calculation}>{calculation}</p>
+  <p className={styles.result}>{result || 0}</p>
+  {result === "Erreur de calcul" && <p className={styles.errorMessage}>Veuillez entrer une expression valide.</p>}
+</div>
 			<div className={styles.grid}>
 				<button onClick={clearAll}>C</button>
 				<button onClick={toggleSign}>-/+</button>
 				<button onClick={handleOpenParenthesis}>(</button>
 				<button onClick={handleCloseParenthesis}>)</button>
 				<button onClick={handlePercentage}>%</button>
-				<button onClick={handleSquareRoot}>√</button>
+				<button onClick={handleSquareRoot} disabled={isNegative()}>√</button>
 				<button onClick={handleSquare}>x²</button>
-				<button onClick={() => handleOperator("+")}>+</button>
+				<button onClick={() => handleOperator("+")} disabled={!calculation || /[\+\-\*\/]$/.test(calculation)}>+</button>
 				<button onClick={() => handleDigit("7")}>7</button>
 				<button onClick={() => handleDigit("8")}>8</button>
 				<button onClick={() => handleDigit("9")}>9</button>
-				<button onClick={() => handleOperator("-")}>-</button>
+				<button onClick={() => handleOperator("-")} disabled={!calculation || /[\+\-\*\/]$/.test(calculation)}>-</button>
 				<button onClick={() => handleDigit("4")}>4</button>
 				<button onClick={() => handleDigit("5")}>5</button>
 				<button onClick={() => handleDigit("6")}>6</button>
-				<button onClick={() => handleOperator("*")}>x</button>
+				<button onClick={() => handleOperator("*")} disabled={!calculation || /[\+\-\*\/]$/.test(calculation)}>x</button>
 				<button onClick={() => handleDigit("1")}>1</button>
 				<button onClick={() => handleDigit("2")}>2</button>
 				<button onClick={() => handleDigit("3")}>3</button>
-				<button onClick={() => handleOperator("/")}>/</button>
+				<button onClick={() => handleOperator("/")} disabled={!calculation || /[\+\-\*\/]$/.test(calculation)}>/</button>
 				<button onClick={() => handleDigit("0")}>0</button>
 				<button onClick={handleDecimal}>.</button>
-				<button onClick={calculateResult}>=</button>
+                <button onClick={calculateResult} className={styles.resultButton}>=</button>
+
 			</div>
 		</div>
 	);
